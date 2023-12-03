9443a39aa8490e44652eaa10ef020201c06659df
Jhony Ortiz <86069583+ortizjhony@users.noreply.github.com>
2023-12-03 17:14:02 -0500

    Update SampleData2.sql

--- SQL/SampleData2.sql
+++ SQL/SampleData2.sql
@@ -1,15 +1,16 @@
 -- Inserting sample personnel data including physicians, surgeons, nurses, and support staff
-INSERT INTO Personnel (Name, Gender, Address, TelephoneNumber, Salary, Role) VALUES
-('Dr. Alice Monroe', 'F', '123 Wellness Road', '555-0101', 150000,'Physician'), -- Physician
-('Dr. Bob Wallace', 'M', '456 Health Ave', '555-0202', 160000,'Physician'),     -- Physician
-('Dr. Carol Hughes', 'F', '789 Care Blvd', '555-0303', NULL,'Surgeon'),        -- Surgeon
-('Dr. Derek Long', 'M', '321 Cure Street', '555-0404', NULL,'Surgeon'),        -- Surgeon
-('Nurse Emma Walsh', 'F', '654 Treat Road', '555-0505', 70000,'Nurse'),      -- Nurse
-('Nurse Jackie Ford', 'F', '128 Bandaid Ave', '555-1236', 75000,'Nurse'),      -- Nurse
-('Mark Spencer', 'M', '132 Aid Lane', '555-0606', 50000,'Support Staff'),            -- Support Staff
-('Nora Kaye', 'F', '975 Assist Terrace', '555-0707', 45000,'Support Staff');         -- Support Staff
-
-
+INSERT INTO Personnel 
+(Name, Gender, Address, TelephoneNumber, Salary, Role, Specialty, Grade, YearsExperience,ContractType,ContractLength ) VALUES
+('Dr. Alice Monroe', 'F', '123 Wellness Road', '555-0101', 150000,'Physician','Geriatric',NULL,NULL,NULL,NULL), -- Physician
+('Dr. Bob Wallace', 'M', '456 Health Ave', '555-0202', 160000,'Physician','Primary Care',NULL,NULL,NULL,NULL),     -- Physician
+('Dr. Carol Hughes', 'F', '789 Care Blvd', '555-0303', NULL,'Surgeon','Orthopedist',NULL,NULL, 'Independent',3),        -- Surgeon
+('Dr. Derek Long', 'M', '321 Cure Street', '555-0404', NULL,'Surgeon','Cardiologist',NULL,NULL,'Independent',2),        -- Surgeon
+('Nurse Emma Walsh', 'F', '654 Treat Road', '555-0505', 70000,'Nurse',NULL,5,3,NULL,NULL),      -- Nurse
+('Nurse Jackie Ford', 'F', '128 Bandaid Ave', '555-1236', 75000,'Nurse',NULL,7,12,NULL,NULL),      -- Nurse
+('Mark Spencer', 'M', '132 Aid Lane', '555-0606', 50000,'Support Staff',NULL,NULL,NULL,NULL,NULL),            -- Support Staff
+('Nora Kaye', 'F', '975 Assist Terrace', '555-0707', 45000,'Support Staff',NULL,NULL,NULL,NULL,NULL),      -- Support Staff
+('Henry Kayne', 'F', '975 Living Terrace', '555-1122', 150000,'Chief of Staff',NULL,NULL,NULL,NULL,NULL);         -- Chief of Staff
+    
 -- Inserting surgery skills
 INSERT INTO SurgerySkill (Description) VALUES
 ('General Surgery'),
@@ -24,7 +25,7 @@ INSERT INTO SurgerySkill (Description) VALUES
 INSERT INTO NurseSkill (NurseID, SkillCode) VALUES
 (5, 1),
 (5, 2),
-(5, 3);
+(5, 3),
 (6, 4);
 
 -- Inserting surgery types
@@ -59,10 +60,10 @@ INSERT INTO MedicationInteraction (MedicationCode1, MedicationCode2, Severity) V
 
 
 -- Inserting patients
-INSERT INTO Patient (Name, Gender, DateOfBirth, Address, TelephoneNumber, SocialSecurityNumber, PrimaryPhysician) VALUES
-('James Smith', 'M', '1984-06-15', '123 Patient Street', '555-1111', '111-22-3333', 1),
-('Linda Taylor', 'F', '1979-08-25', '456 Recovery Road', '555-2222', '222-33-4444', 2),
-('Ethan Johnson', 'M', '1995-04-12', '789 Healing Way', '555-3333', '333-44-5555', NULL); -- No primary physician yet
+INSERT INTO Patient (Name, Gender, DateOfBirth, Address, TelephoneNumber, SocialSecurityNumber, PrimaryPhysician, BloodType) VALUES
+('James Smith', 'M', '1984-06-15', '123 Patient Street', '555-1111', '111-22-3333', 1,'A'),
+('Linda Taylor', 'F', '1979-08-25', '456 Recovery Road', '555-2222', '222-33-4444', 2,'B'),
+('Ethan Johnson', 'M', '1995-04-12', '789 Healing Way', '555-3333', '333-44-5555', NULL,'AB'); -- No primary physician yet
 
 
 -- Inserting surgery schedules
@@ -100,15 +101,47 @@ INSERT INTO Consultation (PatientNumber, PhysicianNumber, ConsultationDateTime, 
 
 -- Inserting medical measurements
 -- ConsultationID corresponds to previous insertions
-INSERT INTO MedicalMeasurement (ConsultationID, BloodType, CholesterolHDL, CholesterolLDL, Triglyceride, BloodSugar) VALUES
-(1, 'O', 55, 120, 200, 110), -- This would trigger a calculation for heart disease risk
-(2, 'A', 65, 130, 180, 90);  -- This would trigger a calculation for heart disease risk
+INSERT INTO MedicalData (ConsultationID, CholesterolHDL, CholesterolLDL, Triglyceride, BloodSugar) VALUES
+(1, 55, 120, 200, 110), -- This would trigger a calculation for heart disease risk
+(2, 65, 130, 180, 90);  -- This would trigger a calculation for heart disease risk
 
 
 -- Inserting JobShift
 INSERT INTO JobShift (EmploymentNumber, ShiftDate, ShiftStart, ShiftEnd) VALUES
-(4, '2023-12-01', '08:00', '17:00');
-(5, '2023-12-01', '12:00', '21:00');
-(6, '2023-12-01', '20:00', '06:00');
-(7, '2023-12-01', '08:00', '17:00');
+(4, '2023-12-01', '08:00', '17:00'),
+(5, '2023-12-01', '12:00', '21:00'),
+(6, '2023-12-01', '20:00', '06:00'),
+(7, '2023-12-01', '08:00', '17:00'),
 (8, '2023-12-01', '12:00', '21:00');
+
+
+INSERT INTO Illness (Description) VALUES
+('Influenza'),
+('Hypertension'),
+('Diabetes'),
+('Asthma');
+
+-- Assuming PatientNumber 1 has Influenza (IllnessCode 1) and Hypertension (IllnessCode 2)
+INSERT INTO PatientIllness (ConsultationID, IllnessCode) VALUES
+((SELECT ConsultationID FROM Consultation WHERE PatientNumber = 1 LIMIT 1), 1),
+((SELECT ConsultationID FROM Consultation WHERE PatientNumber = 1 LIMIT 1), 2);
+
+-- Assuming PatientNumber 2 has Diabetes (IllnessCode 3)
+INSERT INTO PatientIllness (ConsultationID, IllnessCode) VALUES
+((SELECT ConsultationID FROM Consultation WHERE PatientNumber = 2 LIMIT 1), 3);
+
+
+INSERT INTO InPatient (PatientNumber, DateOfAdmission, NursingUnit, AttendingNurse) VALUES
+(1, '2023-11-01', 3, 5),  -- Patient 1 admitted on Nov 1, 2023, in Nursing Unit 3 with Attending Nurse 5
+(2, '2023-11-02', 4, 6),  -- Patient 2 admitted on Nov 2, 2023, in Nursing Unit 4 with Attending Nurse 6
+(3, '2023-11-03', 5, 7);  -- Patient 3 admitted on Nov 3, 2023, in Nursing Unit 5 with Attending Nurse 7
+
+INSERT INTO Room (RoomNumber, BedNumber, Occupied, CurrentPatient) VALUES
+('101A', 'A', TRUE, 1),   -- Room 101A, Bed A is occupied by InPatient 1
+('101A', 'B', FALSE, NULL),-- Room 101A, Bed B is unoccupied
+('102B', 'A', TRUE, 2),   -- Room 102B, Bed A is occupied by InPatient 2
+('102B', 'B', FALSE, NULL),-- Room 102B, Bed B is unoccupied
+('103C', 'A', FALSE, NULL),-- Room 103C, Bed A is unoccupied
+('103C', 'B', FALSE, NULL);-- Room 103C, Bed B is unoccupied
+
+
